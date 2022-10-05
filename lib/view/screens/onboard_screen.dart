a127f8691c477b72835c9483af646a6b9572c63d
Taha Yusuf Yıldız <tyusufyildiz71@gmail.com>
2022-10-05 17:58:10 +0300

    onboard screen

--- lib/view/screens/onboard_screen.dart
+++ lib/view/screens/onboard_screen.dart
@@ -1,9 +1,127 @@
+import 'package:chat_app/view/screens/login_screen.dart';
+import 'package:chat_app/view/screens/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_onboard/flutter_onboard.dart';
 
 import '../../constatnt/onboard_components/login_button.dart';
 import '../../constatnt/onboard_components/register_button.dart';
 
 class OnboardPage extends StatefulWidget {
+  const OnboardPage({super.key});
+
+  @override
+  State<OnboardPage> createState() => _OnboardPageState();
+}
+
+class _OnboardPageState extends State<OnboardPage> {
+  final PageController _pageController = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OnBoard(
+        pageController: _pageController,
+        // Either Provide onSkip Callback or skipButton Widget to handle skip state
+        onSkip: () {
+          // print('skipped');
+        },
+        // Either Provide onDone Callback or nextButton Widget to handle done state
+        onDone: () {
+          // print('done tapped');
+        },
+        onBoardData: onBoardData,
+        titleStyles: const TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.w900,
+          letterSpacing: 0.15,
+        ),
+        descriptionStyles: TextStyle(
+          fontSize: 16,
+          color: Colors.brown.shade300,
+        ),
+        pageIndicatorStyle: const PageIndicatorStyle(
+          width: 100,
+          inactiveColor: Colors.black,
+          activeColor: Colors.deepOrange,
+          inactiveSize: Size(8, 8),
+          activeSize: Size(12, 12),
+        ),
+        // Either Provide onSkip Callback or skipButton Widget to handle skip state
+        skipButton: TextButton(
+          onPressed: () {
+            // print('skipButton pressed');
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LoginScreen()));
+          },
+          child: const Text(
+            "Skip",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        // Either Provide onDone Callback or nextButton Widget to handle done state
+        nextButton: OnBoardConsumer(
+          builder: (context, ref, child) {
+            final state = ref.watch(onBoardStateProvider);
+            return InkWell(
+              onTap: () => state.isLastPage
+                  ? Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()))
+                  : _onNextTap(state),
+              child: Container(
+                width: 230,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(32),
+                    color: Colors.black),
+                child: Text(
+                  state.isLastPage ? "Done" : "Next",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _onNextTap(OnBoardState onBoardState) {
+    if (!onBoardState.isLastPage) {
+      _pageController.animateToPage(
+        onBoardState.page + 1,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOutSine,
+      );
+    } else {
+      //print("nextButton pressed");
+    }
+  }
+}
+
+final List<OnBoardModel> onBoardData = [
+  const OnBoardModel(
+    title: "Meet new people",
+    description: "KarıSepeti lets you meet and relate to new people",
+    imgUrl: "assets/kadın1.jpg",
+  ),
+  const OnBoardModel(
+    title: "Çok düşünme güzelim düşüneni sevmezler",
+    description:
+        "Bazen sadece yorgun oluyor insan . Ne küs , ne yalnız , ne de aşık.....",
+    imgUrl: 'assets/kadın2.jpg',
+  ),
+  const OnBoardModel(
+    title: "Sarışının adı esmerin tadı",
+    description: "Esmerler kıskanılmasaydı solaryum icat edilmezdi...",
+    imgUrl: 'assets/kadın3.jpg',
+  ),
+];
+
+/*class OnboardPage extends StatefulWidget {
   const OnboardPage({Key? key}) : super(key: key);
 
   @override
@@ -83,4 +201,4 @@ class _OnboardPageState extends State<OnboardPage> {
           )),
     );
   }
-}
+} */
